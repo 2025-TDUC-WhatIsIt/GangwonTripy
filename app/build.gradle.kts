0d10e4c734b697d0c71173c090c9da6f0575718c
jhkim <juho1589@gmail.com>
2025-09-07 16:40:54 +0900

    <feat> 뱃지 시스템 api 연동

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,6 @@ android {
 }
 
 dependencies {
-    // 버전 카탈로그(libs.*)만 사용하도록 정리
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -76,7 +75,13 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:5.1.0")
-
+    implementation ("androidx.camera:camera-core:1.3.4")
+    implementation ("androidx.camera:camera-camera2:1.3.4")
+    implementation ("androidx.camera:camera-lifecycle:1.3.4")
+    implementation ("androidx.camera:camera-view:1.3.4")
+    implementation ("com.google.mlkit:barcode-scanning:17.2.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/gangwontripy/data/api/ApiClient.java
@@ -0,0 +1,29 @@
+package com.example.gangwontripy.data.api;
+
+import com.example.gangwontripy.BuildConfig;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static VisitApi visitApi;
+    private static final String API_BASE = BuildConfig.API_BASE;
+    public static VisitApi visitApi(){
+        if (visitApi == null){
+            HttpLoggingInterceptor log = new HttpLoggingInterceptor();
+            log.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(log).build();
+            Gson gson = new GsonBuilder().setLenient().create();
+            Retrofit r = new Retrofit.Builder()
+                    .baseUrl(API_BASE)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+            visitApi = r.create(VisitApi.class);
+        }
+        return visitApi;
+    }
+}
