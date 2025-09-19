6eb3d3f4dbc8dd3ad13f8a81d58f0f30573272ad
jhkim <juho1589@gmail.com>
2025-09-19 15:35:23 +0900

    commit

--- app/src/main/java/com/whatisit/gangwontripy/MyApp.java
+++ app/src/main/java/com/whatisit/gangwontripy/MyApp.java
@@ -1,12 +1,16 @@
 package com.whatisit.gangwontripy;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.kakao.sdk.common.util.Utility;
 import com.kakao.vectormap.KakaoMapSdk;
 
 public class MyApp extends Application {
     @Override public void onCreate() {
         super.onCreate();
         KakaoMapSdk.init(this, BuildConfig.KAKAO_MAP_KEY);
+        String keyHash = Utility.INSTANCE.getKeyHash(this);
+        Log.d("KeyHash", "release keyHash = " + keyHash);
     }
 }
